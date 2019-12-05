42074167ebc2acf83bd6b3a10269e970e1886651
congbro <2044smile@naver.com>
2019-12-05 14:35:17 +0900

    결제가 생성(created)되면 알림을 보내는 것으로 구현 준비

--- congtube/orders/signals/post_save.py
+++ congtube/orders/signals/post_save.py
@@ -2,11 +2,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from ..models.order import Order
 from notifications.views.kakao import Notification
+from channels.models import Celebrity
 
 
 @receiver(post_save, sender=Order)  # Sender=Order, Order 테이블에서 post_save 발생 시
-def post_save_order(sender, instance, **kwargs):  # order() 가 입력되면 실행
+def post_save_order(instance, created, **kwargs):  # order() 가 입력되면 실행
+    n = Notification()
+    if created:
+        celebrity = Celebrity.objects.get(channel=instance.channel)
+        n.order_confirmations(celebrity)
+
     if instance.status == 2: # 주문완료 시 카카오 알림 발생
-        n = Notification()
         n.payment_complete(instance.phonenumber, instance.user, instance.product.name, instance.amount)
 
