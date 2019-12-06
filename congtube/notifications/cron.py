affea76823477991c29dbe3c5fb54737c7df7f19
congbro <2044smile@naver.com>
2019-12-06 11:09:18 +0900

    카카오 템플릿 검수 전 검수 후 테스트 진행 시 코드확인

--- congtube/notifications/cron.py
+++ congtube/notifications/cron.py
@@ -3,15 +3,18 @@ from channels.models import Celebrity
 from django.db.models import Q
 from orders.models import Order
 from channels.models import Channel
+from notifications.views.kakao import Notification
 
 
 def order_confirmations():
-    now = timezone.now().strftime('%d')
-    order = Order.objects.filter(
-        Q(created_at__day=now) & Q(status=2).values('channel').order_by('channel').distinct())
-    print(order) # <QuerySet [{'channel': 1}, {'channel': 3}]>
-    for orders in order:
-        idx = orders['channel'] # {'channel': 1}, {'channel': 3} : 1, 3
-        ch = Channel.objects.get(pk=idx)
-        celebrity = Celebrity.objects.get(channel=ch)
-        phonenumber = celebrity.phonenumber
+    n = Notification
+    day = int(timezone.now().strftime('%d'))
+    today_order = Order.objects.filter(
+        Q(created_at__day=day) & Q(status=2)).values('channel').order_by('channel').distinct()
+
+    for torder in today_order:
+        idx = torder['channel'] # 제공자의 ID를 idx에 넣는다.
+        ch = Channel.objects.get(pk=idx) # 제공자의 ID로 제공자(Channel) 테이블에서 정보를 꺼낸다.
+        celebrity = Celebrity.objects.get(channel=ch) # 제공자 <-> 제공자개인정보
+        celebrity_phonenumber = celebrity.phonenumber # 제공자개인정보에서 알림을 보내기 위한 폰 번호
+        n.order_confirmation_alarm(celebrity_phonenumber) # 를 인자값으로 넘겨준다.
