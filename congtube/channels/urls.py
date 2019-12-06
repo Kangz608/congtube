144d435af77e2530e145c2253b77b43d61019dfd
congbro <2044smile@naver.com>
2019-12-06 17:15:21 +0900

    url 암호화

--- congtube/channels/urls.py
+++ congtube/channels/urls.py
@@ -9,7 +9,7 @@ app_name = 'channels'
 urlpatterns = [
     path('', ChannelListView.as_view(), name='list'),
     path('<slug:slug>/', ChannelDetailView.as_view(), name='detail'),
-    path('<slug:slug>/star', StarDetailView.as_view(), name='star'), # /star 추후 어려운 값으로 변경예정
+    path('<slug:slug>/1%22]zon}$e+?6W%3Eq', StarDetailView.as_view(), name='star'), # /star 추후 어려운 값으로 변경예정
     path('<slug:slug>/order/',
          login_required(ChannelOrderView.as_view()), name='order'),
 ]
