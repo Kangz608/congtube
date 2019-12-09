1eb3138c6f745aebc509a1d85581dbc3fda5c0ab
congbro <2044smile@naver.com>
2019-12-09 10:50:24 +0900

    Order_confirmations TEST Date Successed
    Toast 템플릿 검수 후 order_confirmations_alarm() 인자로 celebrity_phonenumber 추가

--- congtube/notifications/cron.py
+++ congtube/notifications/cron.py
@@ -7,16 +7,18 @@ from notifications.views.kakao import Notification
 
 
 def order_confirmations():
-    print('order_confirmations1')
-    n = Notification
+    print('order_confirmations 실행')
+    n = Notification()
     day = int(timezone.now().strftime('%d'))
     today_order = Order.objects.filter(
         Q(created_at__day=day) & Q(status=2)).values('channel').order_by('channel').distinct()
 
     for torder in today_order:
-        print('order_confirmations2')
         idx = torder['channel'] # 제공자의 ID를 idx에 넣는다.
         ch = Channel.objects.get(pk=idx) # 제공자의 ID로 제공자(Channel) 테이블에서 정보를 꺼낸다.
         celebrity = Celebrity.objects.get(channel=ch) # 제공자 <-> 제공자개인정보
         celebrity_phonenumber = celebrity.phonenumber # 제공자개인정보에서 알림을 보내기 위한 폰 번호
-        n.order_confirmations_alarm(celebrity_phonenumber) # 를 인자값으로 넘겨준다.
+        print(celebrity_phonenumber)
+        print('order_confirmations_alarm함수 실행 전')
+        n.order_confirmations_alarm() # celebrity_phonenumber 를 인자값으로 넘겨준다.
+        print('order_confirmations_alarm함수 실행 후')
