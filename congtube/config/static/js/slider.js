eeb562fdbfeee3288e83a33bad0a055d6310c485
jieun <jieun@congtube.com>
2019-12-10 16:09:39 +0900

    메인 배너 동작 방식 변경
    번역, 반응형 등을 위해 코딩으로 변경
    각 배너 디자인(css)을 변경하기 위해 html 추가
    로딩 완료시 해당하는 내용만 보이는 script 작성
    - indicator click event, 배너 넘어갈 때 애니메이션 등 추가예정

--- congtube/config/static/js/slider.js
+++ congtube/config/static/js/slider.js
@@ -5,9 +5,48 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
+
     var instances = M.Carousel.init(elems, {
         fullWidth: true,
         indicators: true,
     });
+    
+
 });
+
+// 윈도우 load시 첫번째 banner contents만 보이게
+window.addEventListener('DOMContentLoaded',function(){
+
+    var elemsItems = document.querySelectorAll('.carousel-item');
+    var activeElemsItem = document.getElementsByClassName('carousel-item active');
+    var activeSlideContents = document.getElementById('banner-conts');
+    var slideContents = document.getElementsByClassName('banner-cont-container');
+    var slideConts = document.getElementsByClassName('banner-contents-wrap')[0].childNodes;
+    var activeConts = [];
+
+    var i = 0;
+    while(i<slideConts.length)
+    {
+        if(slideConts[i].tagName=='DIV'){
+            activeConts.push(slideConts[i].innerHTML)
+        }
+        i++;
+    }
+
+    var sI=0;
+    while (sI<elemsItems.length){  
+        if(elemsItems[sI].className=='carousel-item active'){
+            activeSlideContents.innerHTML = activeConts[sI];
+            activeSlideContents.style.display='block';
+            activeSlideContents.className += 'banner-'+sI+'-wrap'
+            console.log(activeSlideContents)
+            // activeSlideContents.innerHTML = 'activeConts[sI].innerHTML';
+        };
+        sI++;
+    }
+
+});
+
 // });
+
+
