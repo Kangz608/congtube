dbc8aa70bac6265fc0ed39192e81a5ac6db31a13
congbro <2044smile@naver.com>
2019-12-10 10:05:32 +0900

    Bestvideo is missing when merging.

--- congtube/channels/admin/__init__.py
+++ congtube/channels/admin/__init__.py
@@ -1,3 +1,4 @@
 from channels.admin.channel import *
 from channels.admin.product import *
 from channels.admin.tag import *
+from channels.admin.bestvideo import *
--- congtube/channels/models/__init__.py
+++ congtube/channels/models/__init__.py
@@ -1,3 +1,4 @@
 from channels.models.channel import *
 from channels.models.product import *
 from channels.models.tag import *
+from channels.models.bestvideo import *
