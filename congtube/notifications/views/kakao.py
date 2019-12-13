82d6ec0a7cfc47763c9221003de4d52902e07dfe
congbro <2044smile@naver.com>
2019-12-13 17:14:03 +0900

    리팩토링 로그제거

--- congtube/notifications/views/kakao.py
+++ congtube/notifications/views/kakao.py
@@ -13,23 +13,17 @@ class Notification:
         data = f'|"plusFriendId":"콩튜브","templateCode":"ordercompleteend","requestDate":"","recipientList":[|"recipientNo":"{phonenumber}","templateParameter":|"고객명":"{user}","상품명":"{product_name}","결제금액":"{format(amount, ",")}","주문내역 확인하기 버튼":""??]?'
         result1 = data.replace("|", "{")
         result2 = result1.replace("?", "}")
-        response = requests.post(self.url, headers=self.headers, data=result2.encode('utf-8'))
-        print(response.status_code)
-        print(response.text)
-        
+        requests.post(self.url, headers=self.headers, data=result2.encode('utf-8'))
+
     def video_complete(self, phonenumber, user, channel): # 영상제작완료
         data = f'|"plusFriendId":"콩튜브","templateCode":"videocheckk","requestDate":"","recipientList":[|"recipientNo":"{phonenumber}","templateParameter":|"고객명":"{user}","채널명":"{channel}","콩튜브 바로가기 버튼":""??]?'
         result1 = data.replace("|", "{")
         result2 = result1.replace("?", "}")
-        response = requests.post(self.url, headers=self.headers, data=result2.encode('utf-8'))
-        print(response.status_code)
-        print(response.text)
+        requests.post(self.url, headers=self.headers, data=result2.encode('utf-8'))
 
     def payment_cancel(self, phonenumber, user, channel, created_at, cancelreason): # 결제취소
         ctime = str(created_at).split(' ')[0]
         data = f'|"plusFriendId":"콩튜브","templateCode":"ordercancel","requestDate":"","recipientList":[|"recipientNo":"{phonenumber}","templateParameter":|"고객명":"{user}","채널명":"{channel}","주문일시":"{ctime}","취소사유":"{cancelreason}","연락가능시간":"10:00~20:00","카카오톡 콩튜브 채널 버튼":""??]?'
         result1 = data.replace("|", "{")
         result2 = result1.replace("?", "}")
-        response = requests.post(self.url, headers=self.headers, data=result2.encode('utf-8'))
-        print(response.status_code)
-        print(response.text)
+        requests.post(self.url, headers=self.headers, data=result2.encode('utf-8'))
