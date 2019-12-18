4dc6a586f64fb4934ba7da64dec6026d68986aa6
jieun <jieun@congtube.com>
2019-12-18 21:45:18 +0900

    주문내역 반응형 변경
    스크롤 내리면 순차적으로 보이도록 설정
    스크롤이 생기면 top버튼 생성
    top버튼 클릭시 scroll top이 0으로 되게 설정
    : 장고 paginate_by 반응형 변경 방법 체크해야함

--- congtube/config/static/js/profile.js
+++ congtube/config/static/js/profile.js
@@ -7,17 +7,51 @@ document.addEventListener('DOMContentLoaded',function(){
             behavior:'smooth'
         });
     });
-});
 
-document.addEventListener('scroll',function(){
-    
-    if(window.innerWidth<992){
-        if(window.scrollY===0){
-        document.getElementById('top').style.display='none'
-        }else{
-        document.getElementById('top').style.display='block'
+    var orderItem = document.getElementsByClassName('order-list-item');
+    for(var i = 0;i<orderItem.length;i++){
+        if(i>3){
+            orderItem[i].style.display='none';
         }
-    }else{
-        document.getElementById('top').style.display='none'
     }
-})
+
+    document.addEventListener('scroll',toggleOrderItems);
+
+
+    function toggleOrderItems(){
+        var oII = 0;
+        while(oII<orderItem.length){
+            var nextStep = orderItem[oII+3];
+            var prevStep = orderItem[oII-1];
+            if(oII!==0&&prevStep.getBoundingClientRect().top<=0){
+                nextStep.style.display='block';
+            }else if(orderItem[oII].getBoundingClientRect().top>50){
+                orderItem.style.display='none';
+            };
+            oII++;
+        }
+    }
+
+    document.addEventListener('scroll',function(){
+    
+        if(window.innerWidth<992){
+            if(window.scrollY===0){
+            document.getElementById('top').style.display='none';
+            for(var i = 0;i<orderItem.length;i++){
+                if(i>3){
+                    orderItem[i].style.display='none';
+                }
+            };
+            }else{
+            document.getElementById('top').style.display='block'
+            };
+    
+        }else{
+            document.getElementById('top').style.display='none'
+            for(var i = 0;i<orderItem.length;i++){
+                orderItem[i].style.display='block';
+            };
+        }
+    });
+
+});
