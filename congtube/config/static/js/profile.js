92bb0b2d730137031e4ffe585581c5230b1e454d
jieun <jieun@congtube.com>
2019-12-19 19:02:33 +0900

    Pagination mobile 변경

--- congtube/config/static/js/profile.js
+++ congtube/config/static/js/profile.js
@@ -1,57 +1,85 @@
-
 document.addEventListener('DOMContentLoaded',function(){
-    document.getElementById('top').addEventListener('click',function(){
+
+    var oldWidth = window.innerWidth;
+    var oII = 0;
+    var test1 = document.getElementById('paginationContainerM').childNodes;
+    var test2 = test1.nodeName;
+    var test3 = [];
+    if(test2==='A'){
+        var test4=0;
+        while(test4<test1.length){
+            test3.push(test1[test4])
+            test4++;
+        }
+        return;
+
+    }
+    console.log(test3)
+    var WIH = window.innerHeight;
+    var BOH = document.body.offsetHeight;
+
+    if(oldWidth<993){
+        toggleBlock();
+    }else{
+        toggleNone();
+    };
+
+    function toggleBlock (){
+        document.addEventListener('scroll',topScroll);
+        document.getElementById('top').style.display='block';
+    };
+
+    function toggleNone(){
+        document.removeEventListener('scroll',topScroll);
+        document.getElementById('top').style.display='none';
+    };
+
+    function myPageResize(){        
+        if(window.innerWidth!==oldWidth&&window.innerwidth<993){
+            toggleBlock();
+        }else{
+            toggleNone();
+        }
+        oldWidth = window.innerWidth;
+        return;
+    };
+
+    // function toggleOrderItems(){
+    // };
+
+    for(var i=8;i<myPagetml.length;i++){
+        myPagetml[i].style.display='none'
+    }
+
+    function topScroll(){
+
+        var scrollTop = window.scrollY;
+
+        if(scrollTop==(BOH-WIH)){
+            // document.getElementById
+            console.log(myPagetml)
+
+        }
+
+        if(window.scrollY===0){
+            document.getElementById('top').style.display='none';
+        }else{
+            document.getElementById('top').style.display='block';
+        };
+    };
+
+    function scrollToTop (){
         window.scrollTo({
             top:0,
             left:0,
             behavior:'smooth'
         });
-    });
+    };
 
-    var orderItem = document.getElementsByClassName('order-list-item');
-    for(var i = 0;i<orderItem.length;i++){
-        if(i>3){
-            orderItem[i].style.display='none';
-        }
-    }
-
-    document.addEventListener('scroll',toggleOrderItems);
-
-
-    function toggleOrderItems(){
-        var oII = 0;
-        while(oII<orderItem.length){
-            var nextStep = orderItem[oII+3];
-            var prevStep = orderItem[oII-1];
-            if(oII!==0&&prevStep.getBoundingClientRect().top<=0){
-                nextStep.style.display='block';
-            }else if(orderItem[oII].getBoundingClientRect().top>50){
-                orderItem.style.display='none';
-            };
-            oII++;
-        }
-    }
-
-    document.addEventListener('scroll',function(){
     
-        if(window.innerWidth<992){
-            if(window.scrollY===0){
-            document.getElementById('top').style.display='none';
-            for(var i = 0;i<orderItem.length;i++){
-                if(i>3){
-                    orderItem[i].style.display='none';
-                }
-            };
-            }else{
-            document.getElementById('top').style.display='block'
-            };
+    document.getElementById('top').addEventListener('click',scrollToTop);
+    document.addEventListener('resize',myPageResize);
     
-        }else{
-            document.getElementById('top').style.display='none'
-            for(var i = 0;i<orderItem.length;i++){
-                orderItem[i].style.display='block';
-            };
-        }
-    });
+    
 
 });
