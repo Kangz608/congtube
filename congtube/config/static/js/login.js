004ce676fd35230451f6c560460e42931b8f77e4
jieun <jieun@congtube.com>
2019-12-23 14:30:19 +0900

    Pagination 반응형 1차 완료

--- congtube/config/static/js/login.js
+++ congtube/config/static/js/login.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
     let bodyHeight = document.body.clientHeight;
     let windowHeight = window.innerHeight;
-    if(windowHeight>bodyHeight){
-        document.getElementById('footer').style.position='absolute';
-        document.getElementById('footer').style.bottom='0';
-    }else{
-        document.getElementById('footer').style.position='initial';
-        document.getElementById('footer').style.bottom='initial';
-    }
-})
+    window.setTimeout(footerCheck,150);
+    function footerCheck(){
+        if(windowHeight>bodyHeight){
+            document.getElementById('footer').style.position='absolute';
+            document.getElementById('footer').style.bottom='0';
+        }else{
+            document.getElementById('footer').style.position='initial';
+            document.getElementById('footer').style.bottom='initial';
+        }
+    };
+    var oldWidth = window.innerWidth;
+    document.addEventListener('resize',function(){
+        if(window.innerWidth!=oldWidth){
+            window.setTimeout(footerCheck,150);
+        }
+    });
+});
